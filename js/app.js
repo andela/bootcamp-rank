445ce15382f88f3fdb1bcd14ea579da37a4380ed
andela-thomas <thomas.nyambati@andela.com>
2015-11-23 22:30:17 +0300

    add landing page ui

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
-(function(){
-	'use strict';
-	angular
-		.module('btcmprank', ['ngMaterial']);
+(function() {
+  'use strict';
+  window.app = angular.module('btcmprank', [
+    'btcmprank.controllers',
+    'ui.router',
+    'ngMaterial'
+  ]);
+
+  window.app.run(['$rootScope', '$location', '$state', '$mdSidenav',
+    function($rootScope, $location, $state, $mdSidenav) {}
+  ]);
+
+  window.app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise('/404');
+
+      $stateProvider
+        .state('login', {
+          url: '/',
+          controller: 'HomeCtrl',
+          templateUrl: 'views/login.html'
+        })
+        .state('404', {
+          url: '/404',
+          templateUrl: 'views/404.html'
+        });
+      $locationProvider.html5Mode(true);
+    }
+  ]);
+
 })();
