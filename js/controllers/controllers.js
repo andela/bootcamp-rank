5a5ec49fafd325231d2b06db4dd5d9dc4b706df7
hannahkoske <hannah.koske@andela.com>
2015-11-26 21:47:18 +0300

    Make final changes to dashboard

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -190,13 +190,6 @@ angular.module('bootrank.controllers', [])
       console.log('Clicked me');
       window.open(link);
     };
-
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
   }])
   .controller('ProjectCtrl', ['$scope', '$rootScope', '$state', 'Auth', 'Utils',
     function($scope, $rootScope, $state, Auth, Utils) {
